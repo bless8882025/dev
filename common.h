985b648d8c1bb5cffd5269cd68ddc3776435fc4d
bless8882025 <94065982+bless8882025@users.noreply.github.com>
2026-07-25 16:06:42 +0500

    Update common.h

--- common.h
+++ common.h
@@ -17,37 +17,12 @@
 #include <dlfcn.h>
 #include <errno.h>
 #include <algorithm>
+#include <endian.h>   // для htobe64, be64toh
 
 const uint8_t CMD_GET = 'G';
 const uint8_t CMD_PUT = 'P';
 
-static inline uint64_t htobe64(uint64_t x) {
-    return ((uint64_t)htonl(x >> 32) << 32) | htonl(x & 0xffffffff);
-}
-static inline uint64_t be64toh(uint64_t x) {
-    return ((uint64_t)ntohl(x >> 32) << 32) | ntohl(x & 0xffffffff);
-}
-
-bool sendAll(int sockfd, const void* buffer, size_t len) {
-    const char* buf = (const char*)buffer;
-    size_t sent = 0;
-    while (sent < len) {
-        ssize_t n = send(sockfd, buf + sent, len - sent, 0);
-        if (n <= 0) return false;
-        sent += n;
-    }
-    return true;
-}
-
-bool recvAll(int sockfd, void* buffer, size_t len) {
-    char* buf = (char*)buffer;
-    size_t received = 0;
-    while (received < len) {
-        ssize_t n = recv(sockfd, buf + received, len - received, 0);
-        if (n <= 0) return false;
-        received += n;
-    }
-    return true;
-}
+bool sendAll(int sockfd, const void* buffer, size_t len);
+bool recvAll(int sockfd, void* buffer, size_t len);
 
 #endif
